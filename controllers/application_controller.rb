0cf6e9f3ecb64e95c277098b6e4d506fd44a649b
Henrique Ioneda <henriqueioneda@gmail.com>
2017-04-11 19:55:39 -0300

    copiando

--- controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-
-
-  def index
-  	@current_user = current_user.id
-  end
-end
